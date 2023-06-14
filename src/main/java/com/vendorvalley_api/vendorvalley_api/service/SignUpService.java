7fb8ca5bd8faf3fc5b2aed782939afd15c4c5686
boon <aaron25rony@gmail.com>
2023-06-14 05:04:52 -0300

    func param added to service method - test passed

--- src/main/java/com/vendorvalley_api/vendorvalley_api/service/SignUpService.java
+++ src/main/java/com/vendorvalley_api/vendorvalley_api/service/SignUpService.java
@@ -1,8 +1,10 @@
 package com.vendorvalley_api.vendorvalley_api.service;
 
+import com.vendorvalley_api.vendorvalley_api.model.SignUpModel;
+
 public class SignUpService {
 
-    public void signUpVendor() {
+    public void signUpVendor(SignUpModel signUpModel) {
 
     }
 }
