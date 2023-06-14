c3dc6351cb963490a8cfa620cd0016a56e60f954
boon <aaron25rony@gmail.com>
2023-06-14 04:27:08 -0300

    post api failed - expected: 2XX: received: 404

--- src/test/java/com/vendorvalley_api/vendorvalley_api/controller/SignUpControllerTest.java
+++ src/test/java/com/vendorvalley_api/vendorvalley_api/controller/SignUpControllerTest.java
@@ -1,8 +1,14 @@
 package com.vendorvalley_api.vendorvalley_api.controller;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @WebMvcTest(controllers = SignUpController.class) //this annotation is used to test only the web layer of our application.
@@ -13,7 +19,12 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
         )
 public class SignUpControllerTest {
 
+        @Autowired
+        MockMvc mockMvc; //encapsulates all our web app beans(classes with @Component, @Service, @Repository, or @Controller annotations) and makes them available for testing
 
-
+        @Test
+        public void signUpVendorTest() throws Exception {
+                mockMvc.perform(post("/signup")).andExpect(status().is2xxSuccessful());
+        }
 
 }
