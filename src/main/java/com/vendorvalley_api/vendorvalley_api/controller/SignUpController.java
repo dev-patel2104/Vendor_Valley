6d5755d006695f7c63cd8b095831a64040b4c6ad
boon <aaron25rony@gmail.com>
2023-06-14 04:48:39 -0300

    post api added to pass the test

--- src/main/java/com/vendorvalley_api/vendorvalley_api/controller/SignUpController.java
+++ src/main/java/com/vendorvalley_api/vendorvalley_api/controller/SignUpController.java
@@ -1,4 +1,14 @@
 package com.vendorvalley_api.vendorvalley_api.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SignUpController {
+
+
+    @PostMapping("/signup")
+    public void signUpVendor() {
+        return;
+    }
 }
