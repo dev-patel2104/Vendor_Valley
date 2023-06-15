dd2f9f54f7538e9dc8c39fcb947f2ed76d818d82
boon <aaron25rony@gmail.com>
2023-06-15 19:22:24 -0300

    databaseConnector doesnt have connect method: test failed

---
+++ src/main/java/com/vendorvalley_api/vendorvalley_api/databaseConnector/DatabaseConnector.java
@@ -0,0 +1,5 @@
+package com.vendorvalley_api.vendorvalley_api.databaseConnector;
+
+public class DatabaseConnector {
+
+}
--- src/test/java/com/vendorvalley_api/vendorvalley_api/repository/SignUpRepositoryTest.java
+++ src/test/java/com/vendorvalley_api/vendorvalley_api/repository/SignUpRepositoryTest.java
@@ -1,18 +1,27 @@
 package com.vendorvalley_api.vendorvalley_api.repository;
 
+import com.vendorvalley_api.vendorvalley_api.databaseConnector.DatabaseConnector;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 @ExtendWith(SpringExtension.class)
 public class SignUpRepositoryTest {
 
+    @Mock
     DatabaseConnector databaseConnector;
 
     @Test
     public void saveVendorTest() {
+        assertNotNull(databaseConnector);
+
+        Connection connection = databaseConnector.connect();
     }
 
 
