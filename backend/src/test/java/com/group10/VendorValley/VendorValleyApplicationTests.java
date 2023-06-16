11109262f5c57e405b62f099420aad4e2316769c
Abhinav1331a <abhinava465@gmail.com>
2023-06-16 17:41:10 -0300

    initial commit with boiler template code

---
+++ backend/src/test/java/com/group10/VendorValley/VendorValleyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group10.VendorValley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorValleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
