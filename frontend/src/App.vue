1c5a783f3935cd935af7b9f1094fa044b6224ebe
Abhinav1331a <abhinava465@gmail.com>
2023-06-16 21:18:54 -0300

    added frontend boilerplate code

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template> 
+<div class="w-100 bg-black text-center p-2">
+  <h3 class="text-white">Vendor Valley</h3>
+</div>
+
+<div>
+  <VendorHome />
+</div>
+</template>
+
+<script setup>
+import VendorHome from '@/components/VendorHome.vue'
+</script>
