55dff284d852df93e8328db54c3a3ccb292408dd
Vishnu Narayanbhai Vasita <vs369825@dal.ca>
2023-06-18 00:07:46 -0300

    login and home frontend

--- frontend/src/components/VendorHome.vue
+++ frontend/src/components/VendorHome.vue
@@ -1,4 +1,37 @@
 <template>
-<h1 class="text-center">Welcome Home</h1>
+    <div class="w-100 bg-black px-4 py-2 d-flex justify-content-between">
+        <div>
+           <h3 @click="goHome" class="Home-Text">Vendor Valley</h3>
+          
+        </div>
+    <div class="d-flex justify-content-center">
+        <router-link to="/login" class="btn btn-light px-4 mx-2">Login</router-link>
+    </div>  
+</div>
 </template>
-<script></script>
+
+<script>
+import router from '@/router'
+export default {
+    name: "NavBar",
+
+    methods: {
+        goHome() {
+            router.push('/')
+        }
+    }
+}
+
+
+</script>
+
+<style scoped>
+.Home-Text{
+    color: white;
+
+}
+
+.Home-Text:hover {
+    cursor: pointer;
+}
+</style>
