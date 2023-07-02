0c06fcdfa8504daddd29d3561d2675bfca4558ed
Dev Patel <Dev.Patel@dal.ca>
2023-07-02 01:20:07 -0300

    Added code for the frontend part of the sign up user and vendor.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from "@/views/Home.vue"
 import Login from '@/views/Login.vue'
+import Signup from '@/views/Signup.vue'
+
 
 const routes = [
   {path: '/', component: Home},
-  {path: '/login', component: Login}
+  {path: '/login', component: Login},
+  {path: '/signup', component: Signup},
 ]
-
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
