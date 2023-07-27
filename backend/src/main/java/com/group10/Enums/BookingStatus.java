a68769982dab3e489830dbd47b791ec2d4024656
boon <aaron25rony@gmail.com>
2023-07-27 20:07:26 -0300

    documented bookingutil and bookingstatus enum file

--- backend/src/main/java/com/group10/Enums/BookingStatus.java
+++ backend/src/main/java/com/group10/Enums/BookingStatus.java
@@ -1,17 +1,41 @@
 package com.group10.Enums;
 
+/**
+ * Enum representing the status of a booking request.
+ */
 public enum BookingStatus {
 
+    /**
+     * The booking request is awaiting approval.
+     */
     AWAITING("awaiting"),
+
+    /**
+     * The booking request has been accepted.
+     */
     ACCEPTED("accepted"),
+
+    /**
+     * The booking request has been declined.
+     */
     DECLINED("declined");
 
     private String bookingStatus;
 
+    /**
+     * Constructor for the BookingStatus enum.
+     *
+     * @param status The status string associated with the enum constant.
+     */
     BookingStatus(String status) {
         this.bookingStatus = status;
     }
 
+    /**
+     * Get the status string associated with the enum constant.
+     *
+     * @return The status string.
+     */
     public String getBookingStatus() {
         return bookingStatus;
     }
--- backend/src/main/java/com/group10/Util/BookingUtil.java
+++ backend/src/main/java/com/group10/Util/BookingUtil.java
@@ -2,13 +2,23 @@ package com.group10.Util;
 
 import com.group10.Enums.BookingStatus;
 
+/**
+ * Utility class for handling booking-related operations.
+ */
 public class BookingUtil {
 
+    /**
+     * Checks if the given booking status is valid.
+     *
+     * @param bookingStatus The booking status string to check.
+     * @return true if the booking status is valid, false otherwise.
+     */
     public static boolean isValidBookingStatus(String bookingStatus) {
-        for(BookingStatus status: BookingStatus.values()) {
-            if (status.getBookingStatus().equals(bookingStatus)) return true;
+        for (BookingStatus status : BookingStatus.values()) {
+            if (status.getBookingStatus().equals(bookingStatus)) {
+                return true;
+            }
         }
         return false;
     }
-
 }
