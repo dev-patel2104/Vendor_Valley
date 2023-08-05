7cb1205179983162f91436cc6e039a63a0f360b1
boon <aaron25rony@gmail.com>
2023-08-04 23:27:56 -0300

    added javaDoc and logs to few classes in Service package

--- backend/src/main/java/com/group10/Service/AuthenticationServiceImpl.java
+++ backend/src/main/java/com/group10/Service/AuthenticationServiceImpl.java
@@ -12,12 +12,14 @@ import com.group10.Service.Interfaces.IAuthenticationService;
 import com.group10.Util.EmailUtil;
 import com.group10.Util.PasswordUtil;
 import com.group10.Util.StringUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
 
 @Service
+@Slf4j
 public class AuthenticationServiceImpl implements IAuthenticationService
 {
     @Autowired
@@ -29,74 +31,52 @@ public class AuthenticationServiceImpl implements IAuthenticationService
     private final int IS_VENDOR = 1;
     private final int IS_NOT_VENDOR = 0;
 
-    public boolean SignIn(SignUpModel signUpModel) throws UserAlreadyPresentException, SQLException, VendorDetailsAbsentForUserException {
-        if (signUpModel == null) {
-            return false;
-        }
-        if (!StringUtil.isNotNullAndNotEmpty(signUpModel.getFirstName())) {
-            return false;
-        }
-        if (!StringUtil.isNotNullAndNotEmpty(signUpModel.getLastName())) {
-            return false;
-        }
-        if (!StringUtil.isNotNullAndNotEmpty(signUpModel.getMobile())) {
-            return false;
-        }
-        if(signUpModel.getIsVendor() != IS_NOT_VENDOR && signUpModel.getIsVendor() != IS_VENDOR) {
-            return false;
-        }
-        if (!StringUtil.isNotNullAndNotEmpty(signUpModel.getStreet())) {
-            return false;
-        }
-        if (!StringUtil.isNotNullAndNotEmpty(signUpModel.getCity())) {
-            return false;
-        }
-        if (!StringUtil.isNotNullAndNotEmpty(signUpModel.getProvince())) {
-            return false;
-        }
-        if (!StringUtil.isNotNullAndNotEmpty(signUpModel.getCountry())) {
-            return false;
-        }
-        if (!StringUtil.isNotNullAndNotEmpty(signUpModel.getEmail())) {
-            return false;
-        }
-        if(!PasswordUtil.isValidPassword(signUpModel.getPassword())) {
+    /**
+     * Signs up a user with the provided sign-up model.
+     *
+     * @param signUpModel The sign-up model containing user information.
+     * @return True if the user registration is successful, false if validation fails or registration fails.
+     * @throws UserAlreadyPresentException If the user is already registered.
+     * @throws SQLException If there is an error while accessing the database.
+     * @throws VendorDetailsAbsentForUserException If vendor details are absent for a vendor user.
+     */
+    public boolean SignIn(SignUpModel signUpModel) throws UserAlreadyPresentException, VendorDetailsAbsentForUserException, SQLException {
+        // Validate the sign-up model
+        if (validateSignUpModel(signUpModel)) {
             return false;
         }
 
-        if(!EmailUtil.isValidEmail(signUpModel.getEmail())) {
-            return false;
-        }
-
-        if(signUpModel.getIsVendor() == IS_VENDOR)
-        {
-            if(VendorRepositoryImpl.addUser(signUpModel))
-            {
-                return true;
+        try {
+            if (signUpModel.getIsVendor() == IS_VENDOR) {
+                // Register vendor user
+                if (VendorRepositoryImpl.addUser(signUpModel)) {
+                    log.info("Vendor user registered successfully: {}", signUpModel.getEmail());
+                    return true;
+                } else {
+                    throw new VendorDetailsAbsentForUserException("User details saved to db, but vendor details are missing in the database");
+                }
+            } else {
+                // Register customer user
+                if (CustomerRepositoryImpl.addUser(signUpModel)) {
+                    log.info("Customer user registered successfully: {}", signUpModel.getEmail());
+                    return true;
+                } else {
+                    throw new UserAlreadyPresentException("User Already Exists!");
+                }
             }
-            else {
-                throw new VendorDetailsAbsentForUserException("User details saved to db, but vendor details are missing in database");
-            }
-
+        } catch (SQLException e) {
+            log.error("Error while signing up user: {}", e.getMessage());
+            throw new SQLException(e.getMessage());
         }
-        else
-        {
-            if(CustomerRepositoryImpl.addUser(signUpModel)){
-                return true;
-            }
-            else{
-                throw new UserAlreadyPresentException("User Already Exists!");
-            }
-
-        }
-
     }
+
+
     /**
      * Logs in a user with the given email and password.
      *
      * @param email The email of the user.
      * @param password The password of the user.
-     * @return The logged in user.
+     * @return The logged-in user.
      * @throws UserDoesntExistException If the user with the given email doesn't exist.
      * @throws InvalidPasswordException If the password entered is incorrect.
      * @throws SQLException If there is an error while accessing the database.
@@ -110,17 +90,84 @@ public class AuthenticationServiceImpl implements IAuthenticationService
              * @return The user entity associated with the given email, or null if no user is found.
              */
             user = CustomerRepositoryImpl.findByEmail(email);
-        }
-        catch (SQLException e){
+            log.info("User retrieved from the repository for email: {}", email);
+        } catch (SQLException e) {
+            log.error("Error while retrieving user from the repository: {}", e.getMessage());
             throw new SQLException(e.getMessage());
         }
+
         if (user == null) {
-            throw new UserDoesntExistException("User Doesn't Exists!");
+            log.warn("User with email {} doesn't exist.", email);
+            throw new UserDoesntExistException("User Doesn't Exist!");
         }
-        if (!user.getPassword().equalsIgnoreCase(password)){
+
+        if (!user.getPassword().equalsIgnoreCase(password)) {
+            log.warn("Invalid password provided for user with email: {}", email);
             throw new InvalidPasswordException("Wrong Password Entered!");
         }
+
+        log.info("User logged in successfully: {}", email);
         return user;
     }
 
+
+    /**
+     * Validates the provided sign-up model.
+     *
+     * @param signUpModel The sign-up model to validate.
+     * @return True if the sign-up model is invalid, false if it's valid.
+     */
+    private boolean validateSignUpModel(SignUpModel signUpModel) {
+        if (signUpModel == null) {
+            log.warn("Sign-up model is null.");
+            return true;
+        }
+        if (!StringUtil.isNotNullAndNotEmpty(signUpModel.getFirstName())) {
+            log.warn("First name is null or empty.");
+            return true;
+        }
+        if (!StringUtil.isNotNullAndNotEmpty(signUpModel.getLastName())) {
+            log.warn("Last name is null or empty.");
+            return true;
+        }
+        if (!StringUtil.isNotNullAndNotEmpty(signUpModel.getMobile())) {
+            log.warn("Mobile is null or empty.");
+            return true;
+        }
+        if (signUpModel.getIsVendor() != IS_NOT_VENDOR && signUpModel.getIsVendor() != IS_VENDOR) {
+            log.warn("Invalid vendor value: {}", signUpModel.getIsVendor());
+            return true;
+        }
+        if (!StringUtil.isNotNullAndNotEmpty(signUpModel.getStreet())) {
+            log.warn("Street is null or empty.");
+            return true;
+        }
+        if (!StringUtil.isNotNullAndNotEmpty(signUpModel.getCity())) {
+            log.warn("City is null or empty.");
+            return true;
+        }
+        if (!StringUtil.isNotNullAndNotEmpty(signUpModel.getProvince())) {
+            log.warn("Province is null or empty.");
+            return true;
+        }
+        if (!StringUtil.isNotNullAndNotEmpty(signUpModel.getCountry())) {
+            log.warn("Country is null or empty.");
+            return true;
+        }
+        if (!StringUtil.isNotNullAndNotEmpty(signUpModel.getEmail())) {
+            log.warn("Email is null or empty.");
+            return true;
+        }
+        if (!PasswordUtil.isValidPassword(signUpModel.getPassword())) {
+            log.warn("Invalid password.");
+            return true;
+        }
+
+        if (!EmailUtil.isValidEmail(signUpModel.getEmail())) {
+            log.warn("Invalid email format: {}", signUpModel.getEmail());
+            return true;
+        }
+
+        return false;
+    }
 }
--- backend/src/main/java/com/group10/Service/CustomerSelectsVendorServiceImpl.java
+++ backend/src/main/java/com/group10/Service/CustomerSelectsVendorServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.List;
 import com.group10.Repository.Interfaces.ICustomerRepository;
 import com.group10.Repository.Interfaces.IReviewsRepository;
 import com.group10.Repository.Interfaces.IServiceRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailAuthenticationException;
 import org.springframework.mail.MailParseException;
@@ -28,6 +29,7 @@ import com.group10.Util.JWTTokenHandler;
  * Service class that handles the logic for when a customer selects a vendor.
  */
 @org.springframework.stereotype.Service
+@Slf4j
 public class CustomerSelectsVendorServiceImpl implements ICustomerSelectsVendorService{
 
     @Autowired
@@ -58,53 +60,26 @@ public class CustomerSelectsVendorServiceImpl implements ICustomerSelectsVendorS
      * @throws Exception If there is an error retrieving the reviews.
      */
     @Override
-    public List<Review> getReviews(String serviceId) throws Exception {
-        try{
-
+    public List<Review> getReviews(String serviceId) throws NumberFormatException, Exception {
+        try {
             int serviceIdInt = Integer.parseInt(serviceId);
             boolean exists = checkIfServiceExists(serviceIdInt);
-            if (!exists){
+            if (!exists) {
+                log.info("No reviews found for service with ID: {}", serviceIdInt);
                 return new ArrayList<>();
             }
-            /**
-             * Retrieves the reviews for a given service.
-             *
-             * @param serviceIdInt The ID of the service to retrieve reviews for.
-             * @return The reviews for the specified service.
-             */
+
+            log.info("Retrieving reviews for service with ID: {}", serviceIdInt);
             return serviceReviewsRepository.getReviewsForService(serviceIdInt);
-        }
-        catch (NumberFormatException e){
-            throw new NumberFormatException("Not a valid service id");
-        }
-        catch (Exception e){
+        } catch (NumberFormatException e) {
+            log.error("Invalid service ID format: {}", serviceId);
+            throw new NumberFormatException("Not a valid service ID");
+        } catch (Exception e) {
+            log.error("Error while retrieving reviews: {}", e.getMessage());
             throw new Exception(e.getMessage());
         }
     }
 
-    /**
-     * Checks if a service with the given service ID exists in the service repository.
-     *
-     * @param serviceId The ID of the service to check for existence.
-     * @return true if the service exists, false otherwise.
-     * @throws SQLException if there is an error while checking for service existence.
-     */
-    private boolean checkIfServiceExists(int serviceId) throws SQLException {
-        boolean exists = false;
-        try{
-            /**
-             * Checks if a service with the given serviceId exists in the service repository.
-             *
-             * @param serviceId The ID of the service to check for existence.
-             * @return true if the service exists, false otherwise.
-             */
-            exists = serviceRepository.checkIfServiceExists(serviceId);
-        }
-        catch(SQLException e){
-            throw new SQLException(e.getMessage());
-        }
-        return exists;
-    }
 
     /**
      * Writes a review for a user with the provided JWT token.
@@ -117,48 +92,39 @@ public class CustomerSelectsVendorServiceImpl implements ICustomerSelectsVendorS
      */
     @Override
     public boolean writeReviews(Review review, String JWTToken) throws SQLException, JWTVerificationException {
-        /**
-         * Decodes a JWT token using the provided JWT token handler.
-         *
-         * @param JWTToken The JWT token to decode
-         * @return The decoded JWT object
-         */
-        // Check if booking exists
-        int bookingId = review.getBookingId();
-        if (bookingId == 0){
-            return false;
+        try {
+            // Check if booking exists
+            int bookingId = review.getBookingId();
+            if (bookingId == 0) {
+                log.warn("Invalid booking ID in review: {}", bookingId);
+                return false;
+            }
+
+            // Decode JWT token
+            DecodedJWT jwt = jwtTokenHandler.decodeJWTToken(JWTToken);
+            int userId = jwt.getClaim("userId").asInt();
+            review.setReviewerId(userId);
+
+            // Check if booking exists and matches user
+            boolean bookingExists = serviceRepository.checkIfBookingExists(bookingId, review.getServiceId(), review.getReviewerId());
+            if (!bookingExists) {
+                log.warn("Booking does not exist or does not match user: bookingId={}, serviceId={}, reviewerId={}",
+                        bookingId, review.getServiceId(), review.getReviewerId());
+                return false;
+            }
+            review.setBookingId(bookingId);
+
+            log.info("Writing review for serviceId={}, reviewerId={}", review.getServiceId(), review.getReviewerId());
+            return startWritingReview(review);
+        } catch (JWTVerificationException e) {
+            log.error("JWT Token verification failed: {}", e.getMessage());
+            throw e;
+        } catch (SQLException e) {
+            log.error("Error while writing review: {}", e.getMessage());
+            throw new SQLException(e.getMessage());
         }
-        DecodedJWT jwt = jwtTokenHandler.decodeJWTToken(JWTToken);
-        // Get user id from JWT Token
-        int userId = jwt.getClaim("userId").asInt();
-        // Set user id in review object
-        review.setReviewerId(userId);
-        boolean bookingExists = serviceRepository.checkIfBookingExists(bookingId, review.getServiceId(), review.getReviewerId());
-        if (!bookingExists){
-            return false;
-        }
-        review.setBookingId(bookingId);
-        return startWritingReview(review);
     }
 
-    private boolean startWritingReview(Review review) throws SQLException {
-        boolean result = false;
-
-        // Create review dateTime
-        LocalDateTime dateTime = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String formattedDateTime = dateTime.format(formatter);
-        // Set review dateTime
-        review.setReviewDate(formattedDateTime);
-        /**
-         * Writes the given review to the service repository.
-         *
-         * @param review The review to be written.
-         * @return The result of the write operation.
-         */
-        result = serviceReviewsRepository.writeReviews(review);
-        return result;
-    }
 
     /**
      * Sends an email with the provided service ID, email text, and JWT token.
@@ -174,45 +140,102 @@ public class CustomerSelectsVendorServiceImpl implements ICustomerSelectsVendorS
      * @throws JWTVerificationException If there is an error verifying the JWT token.
      */
     @Override
-    public boolean sendEmail(String serviceId, String emailText, String JWTToken) throws MailAuthenticationException, MailSendException, MailParseException, SQLException, JWTVerificationException{
-        if (serviceId == null || serviceId.equals("") || JWTToken == null || JWTToken.equals("") || emailText == null || emailText.equals(""))
-        {
+    public boolean sendEmail(String serviceId, String emailText, String JWTToken)
+            throws MailAuthenticationException, MailSendException, MailParseException, SQLException, JWTVerificationException {
+        if (serviceId == null || serviceId.equals("") || JWTToken == null || JWTToken.equals("") || emailText == null || emailText.equals("")) {
             return false;
         }
-        boolean result = false;
-        /**
-         * Decodes a JWT token using the provided JWT token handler.
-         *
-         * @param JWTToken The JWT token to decode
-         * @return The decoded JWT object
-         */
-        DecodedJWT jwt = jwtTokenHandler.decodeJWTToken(JWTToken);
-        // Get user email from JWT Token
-        String senderEmail = jwt.getClaim("email").asString(); 
-        // Get user name from user email
-        User user = CustomerRepositoryImpl.findByEmail(senderEmail);
-        String name = user.getFullName();
-        // Get service name from service id
-        int serviceIdInt = Integer.parseInt(serviceId);
-        Service service = serviceRepository.getServiceDetails(serviceIdInt);
-        if (service == null || user == null){
-            return false;
+
+        try {
+            DecodedJWT jwt = jwtTokenHandler.decodeJWTToken(JWTToken);
+            String senderEmail = jwt.getClaim("email").asString();
+            User user = CustomerRepositoryImpl.findByEmail(senderEmail);
+            String name = user.getFullName();
+            int serviceIdInt = Integer.parseInt(serviceId);
+            Service service = serviceRepository.getServiceDetails(serviceIdInt);
+            if (service == null || user == null) {
+                return false;
+            }
+
+            emailDetails.setSubject("You have a new message from " + name + " regarding your service " + service.getServiceName() + "!");
+            emailDetails.setMsgBody(emailText + "\n\n" + "Sent by: " + senderEmail);
+            emailDetails.setRecipient(service.getCompanyEmail());
+
+            log.info("Sending email from {} to {} regarding service {}", senderEmail, service.getCompanyEmail(), serviceId);
+
+            boolean result = emailUtil.sendSimpleMail(emailDetails);
+
+            if (result) {
+                log.info("Email sent successfully.");
+            } else {
+                log.error("Failed to send email.");
+            }
+
+            return result;
+        } catch (SQLException e) {
+            log.error("Error while sending email: {}", e.getMessage());
+            throw new SQLException("Error while sending email: " + e.getMessage());
         }
-        // Create email details object
-        emailDetails.setSubject("You have a new message from " + name + " regarding your service " + service.getServiceName() + "!");
-        emailDetails.setMsgBody(emailText +"\n\n" + "Sent by: " + senderEmail);
-        emailDetails.setRecipient(service.getCompanyEmail());
-        /**
-         * Sends a simple email using the provided email details.
-         *
-         * @param emailDetails The details of the email to be sent
-         * @return The result of the email sending operation
-         */
-        result = emailUtil.sendSimpleMail(emailDetails);
-        if (!result){
-            return false;
-        }
-        return true;
     }
 
+
+    /**
+     * Checks if a service with the given serviceId exists in the service repository.
+     *
+     * @param serviceId The ID of the service to check for existence.
+     * @return true if the service exists, false otherwise.
+     * @throws SQLException If an error occurs while checking service existence.
+     */
+    private boolean checkIfServiceExists(int serviceId) throws SQLException {
+        boolean exists = false;
+        try {
+            log.info("Checking if service with ID {} exists", serviceId);
+            exists = serviceRepository.checkIfServiceExists(serviceId);
+            log.info("Service with ID {} exists: {}", serviceId, exists);
+        } catch (SQLException e) {
+            log.error("Error while checking service existence: {}", e.getMessage());
+            throw new SQLException(e.getMessage());
+        }
+        return exists;
+    }
+
+
+    /**
+     * Writes the given review to the service repository.
+     *
+     * @param review The review to be written.
+     * @return true if the review is written successfully, false otherwise.
+     * @throws SQLException If an error occurs while writing the review.
+     */
+    private boolean startWritingReview(Review review) throws SQLException {
+        try {
+            // Create review dateTime
+            LocalDateTime dateTime = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String formattedDateTime = dateTime.format(formatter);
+            // Set review dateTime
+            review.setReviewDate(formattedDateTime);
+
+            log.info("Writing review for booking {} and service {}", review.getBookingId(), review.getServiceId());
+
+            /**
+             * Writes the given review to the service repository.
+             *
+             * @param review The review to be written.
+             * @return The result of the write operation.
+             */
+            boolean result = serviceReviewsRepository.writeReviews(review);
+
+            if (result) {
+                log.info("Review successfully written.");
+            } else {
+                log.error("Failed to write review.");
+            }
+
+            return result;
+        } catch (SQLException e) {
+            log.error("Error while writing review: {}", e.getMessage());
+            throw new SQLException("Error while writing review: " + e.getMessage());
+        }
+    }
 }
