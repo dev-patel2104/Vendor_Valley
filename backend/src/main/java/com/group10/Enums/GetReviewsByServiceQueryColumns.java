ebf99a5a081662736d81665242ffdc41ef4cf48f
boon <aaron25rony@gmail.com>
2023-08-04 21:23:40 -0300

    added javaDoc for enums package and missed Exception classes

--- backend/src/main/java/com/group10/Enums/GetReviewsByServiceQueryColumns.java
+++ backend/src/main/java/com/group10/Enums/GetReviewsByServiceQueryColumns.java
@@ -1,23 +1,78 @@
 package com.group10.Enums;
 
+
+/**
+ * Enumeration representing query columns for retrieving reviews by service.
+ * This enum provides a set of values that correspond to various columns used in review queries.
+ */
 public enum GetReviewsByServiceQueryColumns {
+
+    /**
+     * Represents the column name for the service ID associated with the review.
+     */
     SERVICE_ID("service_id"),
+
+    /**
+     * Represents the column name for the user ID associated with the review.
+     */
     USER_ID("user_id"),
+
+    /**
+     * Represents the column name for the booking ID associated with the review.
+     */
     BOOKING_ID("booking_id"),
+
+    /**
+     * Represents the column name for the title of the review.
+     */
     TITLE("title"),
+
+    /**
+     * Represents the column name for the comment text of the review.
+     */
     COMMENT_TEXT("comment_text"),
+
+    /**
+     * Represents the column name for the date the review was created.
+     */
     REVIEW_DATE("review_date"),
+
+    /**
+     * Represents the column name for the rating given in the review.
+     */
     RATING("rating"),
+
+    /**
+     * Represents the column name for the name of the user who submitted the review.
+     */
     NAME("name"),
+
+    /**
+     * Represents the column name for the city of the user who submitted the review.
+     */
     CITY("city"),
+
+    /**
+     * Represents the column name for the country of the user who submitted the review.
+     */
     COUNTRY("country");
 
     private final String columnName;
 
+    /**
+     * Constructs a new GetReviewsByServiceQueryColumns enum with the specified column name.
+     *
+     * @param columnName The name of the query column.
+     */
     GetReviewsByServiceQueryColumns(String columnName) {
         this.columnName = columnName;
     }
 
+    /**
+     * Retrieves the column name associated with the enum value.
+     *
+     * @return The column name.
+     */
     public String getColumnName() {
         return columnName;
     }
--- backend/src/main/java/com/group10/Enums/SearchServiceQueryColumns.java
+++ backend/src/main/java/com/group10/Enums/SearchServiceQueryColumns.java
@@ -1,25 +1,88 @@
 package com.group10.Enums;
 
+
+/**
+ * Enumeration representing query columns for searching services.
+ * This enum provides a set of values that correspond to various columns used in service search queries.
+ */
 public enum SearchServiceQueryColumns {
+
+    /**
+     * Represents the column name for the service ID.
+     */
     SERVICE_ID("service_id"),
+
+    /**
+     * Represents the column name for the user ID associated with the service.
+     */
     USER_ID("user_id"),
+
+    /**
+     * Represents the column name for the name of the service.
+     */
     SERVICE_NAME("service_name"),
+
+    /**
+     * Represents the column name for the description of the service.
+     */
     SERVICE_DESCRIPTION("service_description"),
+
+    /**
+     * Represents the column name for the price of the service.
+     */
     SERVICE_PRICE("service_price"),
+
+    /**
+     * Represents the column name for the categories associated with the service.
+     */
     CATEGORIES("categories"),
+
+    /**
+     * Represents the column name for the street address of the company offering the service.
+     */
     COMPANY_STREET("company_street"),
+
+    /**
+     * Represents the column name for the city of the company offering the service.
+     */
     COMPANY_CITY("company_city"),
+
+    /**
+     * Represents the column name for the province of the company offering the service.
+     */
     COMPANY_PROVINCE("company_province"),
+
+    /**
+     * Represents the column name for the country of the company offering the service.
+     */
     COMPANY_COUNTRY("company_country"),
+
+    /**
+     * Represents the column name for the average rating of the service.
+     */
     AVERAGE_RATING("avgRating"),
+
+    /**
+     * Represents the column name for the total number of bookings for the service.
+     */
     TOTAL_BOOKINGS("totalBookings");
 
     private final String columnName;
 
+    /**
+     * Constructs a new SearchServiceQueryColumns enum with the specified column name.
+     *
+     * @param columnName The name of the query column.
+     */
     SearchServiceQueryColumns(String columnName) {
         this.columnName = columnName;
     }
 
+    /**
+     * Retrieves the column name associated with the enum value.
+     *
+     * @return The column name.
+     */
     public String getColumnName() {
         return columnName;
     }
